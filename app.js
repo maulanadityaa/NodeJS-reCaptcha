12785e02a7c013a79e1da77019c8233f6e6d4493
maulanadityaa <maulanadityaaa@gmail.com>
2021-07-05 16:35:10 +0700

    commit prawan

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.get('/',function(req,res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit',function(req,res){
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
+  }
+  var secretKey = "6LeOmncbAAAAACz5YeeEsEZpFNRbh8pmS1FlQ4H5";
+  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  request(verificationUrl,function(error,response,body) {
+    body = JSON.parse(body);
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+    }
+    res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
+  });
+});
+
+// handle 404
+app.use("*",function(req,res) {
+  res.status(404).send("404");
+})
+
+// localhost:8000
+app.listen(8000);
